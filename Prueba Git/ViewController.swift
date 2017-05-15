84c9370a86587aa9fdc334c51b9bad51da32ab83
Alejandro Cruz <acruz@gotit.com.mx>
2017-05-15 18:05:58 -0500

    Initial Commit

---
+++ Prueba Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba Git
+//
+//  Created by Alejandro Cruz on 15/05/17.
+//  Copyright © 2017 Alejandro Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
